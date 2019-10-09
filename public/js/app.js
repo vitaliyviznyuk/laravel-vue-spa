62edd01dce32f3e36808449cd7246c5f5b2e3bfc
Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
2019-10-09 14:27:35 +0300

    Fetching data before navigation

--- public/js/app.js
+++ public/js/app.js
@@ -1888,35 +1888,108 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
+
+
+var getUsers = function getUsers(page, callback) {
+  var params = {
+    page: page
+  };
+  axios__WEBPACK_IMPORTED_MODULE_0___default.a.get('/api/users', {
+    params: params
+  }).then(function (response) {
+    callback(null, response.data);
+  })["catch"](function (error) {
+    callback(error, error.response.data);
+  });
+};
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
-      loading: false,
       users: null,
+      meta: null,
+      links: {
+        first: null,
+        last: null,
+        next: null,
+        prev: null
+      },
       error: null
     };
   },
-  created: function created() {
-    this.fetchData();
+  computed: {
+    nextPage: function nextPage() {
+      if (!this.meta || this.meta.current_page === this.meta.last_page) {
+        return;
+      }
+
+      return this.meta.current_page + 1;
+    },
+    prevPage: function prevPage() {
+      if (!this.meta || this.meta.current_page === 1) {
+        return;
+      }
+
+      return this.meta.current_page - 1;
+    },
+    paginationCount: function paginationCount() {
+      if (!this.meta) {
+        return;
+      }
+
+      var _this$meta = this.meta,
+          current_page = _this$meta.current_page,
+          last_page = _this$meta.last_page;
+      return "".concat(current_page, " of ").concat(last_page);
+    }
+  },
+  beforeRouteEnter: function beforeRouteEnter(to, from, next) {
+    getUsers(to.query.page, function (err, data) {
+      next(function (vm) {
+        return vm.setData(err, data);
+      });
+    });
+  },
+  // when route changes and this component is already rendered,
+  // the logic will be slightly different.
+  beforeRouteUpdate: function beforeRouteUpdate(to, from, next) {
+    var _this = this;
+
+    this.users = this.links = this.meta = null;
+    getUsers(to.query.page, function (err, data) {
+      _this.setData(err, data);
+
+      next();
+    });
   },
   methods: {
-    fetchData: function fetchData() {
-      var _this = this;
-
-      this.error = this.users = null;
-      this.loading = true;
-      axios__WEBPACK_IMPORTED_MODULE_0___default.a.get('/api/users').then(function (response) {
-        _this.loading = false;
-        _this.users = response.data;
-      })["catch"](function (error) {
-        _this.loading = false;
-        _this.error = error.response.data.message || error.message;
+    goToNext: function goToNext() {
+      this.$router.push({
+        query: {
+          page: this.nextPage
+        }
       });
+    },
+    goToPrev: function goToPrev() {
+      this.$router.push({
+        name: 'users.index',
+        query: {
+          page: this.prevPage
+        }
+      });
+    },
+    setData: function setData(err, _ref) {
+      var users = _ref.data,
+          links = _ref.links,
+          meta = _ref.meta;
+
+      if (err) {
+        this.error = err.toString();
+      } else {
+        this.users = users;
+        this.links = links;
+        this.meta = meta;
+      }
     }
   }
 });
@@ -2523,30 +2596,9 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", { staticClass: "users" }, [
-    _vm.loading
-      ? _c("div", { staticClass: "loading" }, [
-          _vm._v("\n        Loading...\n    ")
-        ])
-      : _vm._e(),
-    _vm._v(" "),
     _vm.error
       ? _c("div", { staticClass: "error" }, [
-          _c("p", [_vm._v(_vm._s(_vm.error))]),
-          _vm._v(" "),
-          _c("p", [
-            _c(
-              "button",
-              {
-                on: {
-                  click: function($event) {
-                    $event.preventDefault()
-                    return _vm.fetchData($event)
-                  }
-                }
-              },
-              [_vm._v("\n                Try Again\n            ")]
-            )
-          ])
+          _c("p", [_vm._v(_vm._s(_vm.error))])
         ])
       : _vm._e(),
     _vm._v(" "),
@@ -2554,6 +2606,7 @@ var render = function() {
       ? _c(
           "ul",
           _vm._l(_vm.users, function(ref) {
+            var id = ref.id
             var name = ref.name
             var email = ref.email
             return _c("li", [
@@ -2565,7 +2618,37 @@ var render = function() {
           }),
           0
         )
-      : _vm._e()
+      : _vm._e(),
+    _vm._v(" "),
+    _c("div", { staticClass: "pagination" }, [
+      _c(
+        "button",
+        {
+          attrs: { disabled: !_vm.prevPage },
+          on: {
+            click: function($event) {
+              $event.preventDefault()
+              return _vm.goToPrev($event)
+            }
+          }
+        },
+        [_vm._v("Previous")]
+      ),
+      _vm._v("\n        " + _vm._s(_vm.paginationCount) + "\n        "),
+      _c(
+        "button",
+        {
+          attrs: { disabled: !_vm.nextPage },
+          on: {
+            click: function($event) {
+              $event.preventDefault()
+              return _vm.goToNext($event)
+            }
+          }
+        },
+        [_vm._v("Next")]
+      )
+    ])
   ])
 }
 var staticRenderFns = []
--- resources/js/views/UsersIndex.vue
+++ resources/js/views/UsersIndex.vue
@@ -1,54 +1,116 @@
 <template>
     <div class="users">
-        <div class="loading" v-if="loading">
-            Loading...
-        </div>
-
         <div v-if="error" class="error">
             <p>{{ error }}</p>
-
-            <p>
-                <button @click.prevent="fetchData">
-                    Try Again
-                </button>
-            </p>
         </div>
 
         <ul v-if="users">
-            <li v-for="{ name, email } in users">
+            <li v-for="{ id, name, email } in users">
                 <strong>Name:</strong> {{ name }},
                 <strong>Email:</strong> {{ email }}
             </li>
         </ul>
+
+        <div class="pagination">
+            <button :disabled="! prevPage" @click.prevent="goToPrev">Previous</button>
+            {{ paginationCount }}
+            <button :disabled="! nextPage" @click.prevent="goToNext">Next</button>
+        </div>
     </div>
 </template>
 <script>
     import axios from 'axios';
+
+    const getUsers = (page, callback) => {
+        const params = { page };
+
+        axios.get('/api/users', { params })
+            .then(response => {
+                callback(null, response.data);
+            }).catch(error => {
+                callback(error, error.response.data);
+            });
+    };
+
     export default {
         data() {
             return {
-                loading: false,
                 users: null,
+                meta: null,
+                links: {
+                    first: null,
+                    last: null,
+                    next: null,
+                    prev: null,
+                },
                 error: null,
             };
         },
-        created() {
-            this.fetchData();
+        computed: {
+            nextPage() {
+                if (! this.meta || this.meta.current_page === this.meta.last_page) {
+                    return;
+                }
+
+                return this.meta.current_page + 1;
+            },
+            prevPage() {
+                if (! this.meta || this.meta.current_page === 1) {
+                    return;
+                }
+
+                return this.meta.current_page - 1;
+            },
+            paginationCount() {
+                if (! this.meta) {
+                    return;
+                }
+
+                const { current_page, last_page } = this.meta;
+
+                return `${current_page} of ${last_page}`;
+            },
+        },
+        beforeRouteEnter (to, from, next) {
+            getUsers(to.query.page, (err, data) => {
+                next(vm => vm.setData(err, data));
+            });
+        },
+        // when route changes and this component is already rendered,
+        // the logic will be slightly different.
+        beforeRouteUpdate (to, from, next) {
+            this.users = this.links = this.meta = null;
+
+            getUsers(to.query.page, (err, data) => {
+                this.setData(err, data);
+                next();
+            });
         },
         methods: {
-            fetchData() {
-                this.error = this.users = null;
-                this.loading = true;
-                axios.get('/api/users')
-                    .then(response => {
-                        this.loading = false;
-                        this.users = response.data;
-                    })
-                    .catch(error => {
-                        this.loading = false;
-                        this.error = error.response.data.message || error.message;
-                    });
-            }
+            goToNext() {
+                this.$router.push({
+                    query: {
+                        page: this.nextPage,
+                    },
+                });
+            },
+            goToPrev() {
+                this.$router.push({
+                    name: 'users.index',
+                    query: {
+                        page: this.prevPage,
+                    }
+                });
+            },
+            setData(err, { data: users, links, meta }) {
+                if (err) {
+                    this.error = err.toString();
+                } else {
+                    this.users = users;
+                    this.links = links;
+                    this.meta = meta;
+                }
+            },
         }
     }
 </script>
